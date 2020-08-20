2b06b601da52eaca35588180b2d0a0fbb2859e70
Larissa Palombo <larissampalombo@gmail.com>
2020-08-20 15:59:15 -0300

    inicial

---
+++ src/Services/API.js
@@ -0,0 +1,15 @@
+// baseado no projeto shopping cart
+const API = 'https://opentdb.com/api_token.php?command=request';
+const OBJ = { method: 'GET' };
+
+export const ResultToken = () => {
+  fetch(API, OBJ)
+    .then(response => response.json())
+    .then(result => localStorage.setItem('token', result.token))
+}; 
+
+export const API_Question = () => {
+  fetch(`https://opentdb.com/api.php?amount=5&token=${localStorage.getItem('token')}`)    
+  .then(response => response.json())
+  .then((result) => localStorage.setItem('question', JSON.stringify(result.results)))
+}
---
+++ src/Services/ApiGravatar.js
@@ -0,0 +1,15 @@
+// referencia https://github.com/brix/crypto-js
+// plantao e slack Pedro Calado
+const CryptoJS = require("crypto-js");
+const API = 'https://www.gravatar.com/avatar/';
+const imagem = 'https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3';
+
+const login => (email) => {
+  const hash = CryptoJS.MD5(email.toLowerCase().toString());
+  if (hash === '') {
+    return imagem;
+  }
+  return `${API}${hash}`;
+}
+
+export default login;
