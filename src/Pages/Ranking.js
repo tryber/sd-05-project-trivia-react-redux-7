0bceebc0dd96c9567af8385ddc930774ecea30c0
Larissa Palombo <larissampalombo@gmail.com>
2020-08-21 19:20:09 -0300

    botao voltar home em components

--- src/Pages/Ranking.js
+++ src/Pages/Ranking.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class ranking extends Component {
+export default class Ranking extends Component {
   render() {
     return (
       <div>
