81e0778f3096a8088696d6da034755e7b90fd6f4
ferrari <luisdiasferrari@gmail.com>
2020-08-21 16:09:38 -0300

    store jogo.

--- src/Components/ComponentsJogo.js
+++ src/Components/ComponentsJogo.js
@@ -1,20 +1,28 @@
 // referência https://github.com/tryber/sd-03-project-trivia-react-redux-05/pull/10
 
-/* import React, { Component } from 'react';
+import React, { Component } from 'react';
+
+botao() {
+  if (correct-answer)
+  return (
+    <div>
+      <button className="button-correct-answer" data-testid="correct-answer"></button>
+    </div>
+  )
+    if (wrong-answer)
+  return (
+    <div>=
+      <button className="button-wrong-answer" data-testid="wrong-answer-${index}"></button>
+    </div>
+  )
+}
 
 export default class button extends Component {
   render() {
-      if (correct-answer)
     return (
       <div>
-        <button className="button-correct-answer" data-testid="correct-answer"></button>
+        
       </div>
-    )
-      if (wrong-answer)
-    return (
-      <div>=
-        <button className="button-wrong-answer" data-testid="wrong-answer-${index}"></button>
-      </div>
-    )
+    )     
   }
-} */
+}
