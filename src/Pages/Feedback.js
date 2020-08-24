e38bf4e75f263197c4b4fcbb245e48f69d3d763d
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-08-24 13:44:01 -0300

    Removendo importação não utilizada.

--- src/Pages/Feedback.js
+++ src/Pages/Feedback.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import Header from '../Components/Header';
 import FeedbackMessage from '../Components/FeedbackMessage';
 
