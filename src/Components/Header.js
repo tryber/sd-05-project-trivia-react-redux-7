4884e66277938968007b346505a4e05fa463aee9
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-08-24 13:32:45 -0300

    Colocando Proptypes e corrigindo code climate.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 // import './Header.css';
 
 const headerStyle = {
@@ -45,25 +46,31 @@ class Header extends Component {
   render() {
     const { imagePath, player, score } = this.props;
     return (
-      <header className='header' style={headerStyle}>
-        <div className='player-avatar' style={playerAvatar}>
+      <header className="header" style={headerStyle}>
+        <div className="player-avatar" style={playerAvatar}>
           <img
             src={imagePath}
-            className='header-player-picture'
-            data-testid='header-profile-picture'
-            alt='Player`s avatar'
+            className="header-player-picture"
+            data-testid="header-profile-picture"
+            alt="Player`s avatar"
             style={imgStyle}
           />
-          <p className={nickName} data-testid='header-player-name'>
+          <p className={nickName} data-testid="header-player-name">
             Jogador: {player}
           </p>
         </div>
-        <p className={scoreStyle} data-testid='header-player-score'>
+        <p className={scoreStyle} data-testid="header-player-score">
           Pontos: {score}
         </p>
       </header>
     );
   }
+}
+
+Header.propTypes = {
+  imagePath: PropTypes.string.isRequired,
+  player: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
 }
 
 export default Header;
