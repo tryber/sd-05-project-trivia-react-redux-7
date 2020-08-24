f162a5e9de0cc31af213b47ce1bdbea4a88beea5
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-08-24 19:10:11 -0300

    Criando o botão de jogar de novo e css

---
+++ src/Components/ButtonPlayAgain.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './ButtonPlayAgain.css';
+
+const PlayAgain = () => {
+  return (
+    <Link to="/" data-testid="btn-play-again" className="button-play-again">Jogar novamente</Link>
+  )
+}
+
+export default PlayAgain;
