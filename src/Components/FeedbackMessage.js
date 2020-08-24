ddd94cc4878e4f7a1e4e2b57707e5423659cb8df
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-08-24 13:59:52 -0300

    Proptypes e Code Climate.

--- src/Components/FeedbackMessage.js
+++ src/Components/FeedbackMessage.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 const Message = (props) => {
   const { message } = props;
   return <p data-testid="feedback-test">{message}</p>;
 };
 
-const TotalScore = (props) => {
-  return (
-    <span data-testid="deedback-total-score">
-      Um total de <span>{props.score}</span> pontos
-    </span>
-  );
-};
+const TotalScore = (props) => (
+  <span data-testid="deedback-total-score">
+    Um total de <span>{props.score}</span> pontos
+  </span>
+);
 
 class FeedbackMessage extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   messageByScore(wins) {
     return wins >= 3 ? 'Mandou bem!' : 'Podia ser melhor...';
   }
@@ -26,7 +21,7 @@ class FeedbackMessage extends Component {
     const { wins } = this.props;
     return (
       <div>
-        <Message message={this.messageByScore(3)} />
+        <Message message={this.messageByScore(wins)} />
         <span>Você acertou {wins} questões!</span>
         <TotalScore score={'50'} />
       </div>
