f5a02149f7134392ca4e10dc4554246406337a87
Larissa Palombo <larissampalombo@gmail.com>
2020-08-24 17:24:36 -0300

    mais cc

--- src/Pages/Config.js
+++ src/Pages/Config.js
@@ -1,12 +1,10 @@
-// A tela de configurações deve possuir um título com o 
-// atributo data-testid contendo o valor settings-title
 import React, { Component } from 'react';
 import '../App.css';
 
 class Config extends Component {
   render() {
-    return <h1 datatest-id="settings-title">Configurações</h1>
-  };
+    return <h1 datatest-id="settings-title">Configurações</h1>;
+  }
 }
 
-  export default Config;
+export default Config;
