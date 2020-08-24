069057692b9f094fb7c5080e3ef528cf4d15b4fb
ferrari <luisdiasferrari@gmail.com>
2020-08-24 19:14:19 -0300

    jogo

--- src/Components/CompBotton.js
+++ src/Components/CompBotton.js
@@ -1,27 +1,27 @@
 // referência https://github.com/tryber/sd-03-project-trivia-react-redux-05/pull/10
 
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class CompBotton extends Component {
-  botao() {
-    if (correct-answer)
+/*   botao() {
+    if (correct-answer) {
+      return (
+        <div>
+          <button className="buttonCorrectAnswer" data-testid="correct-answer">.</button>
+        </div>
+      );
+    }
     return (
       <div>
-        <button className="button-correct-answer" data-testid="correct-answer"></button>
+        <button className="buttonWrongAnswer" data-testid="wrong-answer-${index}">.</button>
       </div>
-    )
-      if (wrong-answer)
-    return (
-      <div>=
-        <button className="button-wrong-answer" data-testid="wrong-answer-${index}"></button>
-      </div>
-    )
-  }
+    );
+  } */
   render() {
     return (
       <div>
-        
+        this.botao
       </div>
-    )
+    );
   }
 }
