96e1b4b440c4502ecf7fb4f10220f9e5cfa6e592
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-08-24 13:35:00 -0300

    Corrigindo mais coisas do CC.

--- src/Pages/Feedback.js
+++ src/Pages/Feedback.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Header from '../Components/Header';
 import FeedbackMessage from '../Components/FeedbackMessage';
 
 export default class feedback extends Component {
   render() {
     return (
-      <div className='container'>
+      <div className="container">
         <Header
           imagePath="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
           player="Jogador Qualquer"
