3ac07603c5119b5d91dddf94791e223ea648cea5
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-08-24 13:12:19 -0300

    Corrigindo CC.

--- src/Components/FeedbackMessage.js
+++ src/Components/FeedbackMessage.js
@@ -2,41 +2,36 @@ import React, { Component } from 'react';
 
 const Message = (props) => {
   const { message } = props;
-  return (
-    <p data-testid="feedback-test">
-      {message}
-    </p>
-  )
-}
+  return <p data-testid='feedback-test'>{message}</p>;
+};
 
 const TotalScore = (props) => {
   return (
     <span data-testid='deedback-total-score'>
       Um total de <span>{props.score}</span> pontos
     </span>
-  )
-}
+  );
+};
 
 class FeedbackMessage extends Component {
   constructor(props) {
-    super (props);
+    super(props);
   }
 
   messageByScore = (wins) => {
-    return (wins >= 3) ? "Mandou bem!": "Podia ser melhor...";
-  }
-  
+    return wins >= 3 ? 'Mandou bem!' : 'Podia ser melhor...';
+  };
+
   render() {
     const { wins } = this.props;
     return (
       <div>
         <Message message={this.messageByScore(3)} />
         <span>Você acertou {wins} questões!</span>
-        <TotalScore score={'50'}/>
+        <TotalScore score={'50'} />
       </div>
-    )
+    );
   }
-
-};
+}
 
 export default FeedbackMessage;
--- src/Pages/Feedback.js
+++ src/Pages/Feedback.js
@@ -5,14 +5,13 @@ import FeedbackMessage from '../Components/FeedbackMessage';
 export default class feedback extends Component {
   render() {
     return (
-      <div className="container">
+      <div className='container'>
         <Header
-          imagePath='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'
-          player='Jogador Qualquer'
-          score='20'
+          imagePath="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+          player="Jogador Qualquer"
+          score="20"
         />
         <FeedbackMessage wins={3} />
-
       </div>
     );
   }
