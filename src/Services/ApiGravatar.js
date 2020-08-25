48443bf62bfe607ae0af42d692c1fc7417a18eb6
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-08-25 17:49:42 -0300

    Esse arquivo ficou obsoleto

--- src/Services/ApiGravatar.js
+++ src/Services/ApiGravatar.js
@@ -4,8 +4,6 @@ import { MD5 } from 'crypto-js';
 
 const API = 'https://www.gravatar.com/avatar/';
 
-const imagem = 'https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3';
-
 const login = (email) => {
     console.log(`${API}${MD5(email.toLowerCase())}`);
     return fetch(`${API}${MD5(email.toLowerCase())}`)
