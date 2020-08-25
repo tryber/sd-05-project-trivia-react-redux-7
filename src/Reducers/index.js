b5f089aa7e7fd083fe94670365b1a0ae5358ae25
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-08-25 17:50:13 -0300

    criando um index para exportar os reducers

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import questionReducer from './questionReducer';
+import loginReducer from './gravatarReducer';
 
 const rootReducer = combineReducers({
   questionReducer,
+  loginReducer,
 });
 export default rootReducer;
