1dcb8863d2456e476759a004e23279e6201ea3b9
Larissa Palombo <larissampalombo@gmail.com>
2020-08-29 14:30:51 -0300

    page ranking

--- src/Pages/Ranking.js
+++ src/Pages/Ranking.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Header from '../Components/Header';
 
 export default class Ranking extends Component {
   render() {
     return (
       <div>
-        .
+        <Header />
+        <title data-testid="ranking-title">Ranking</title>
+        <button type="button" data-testid="btn-go-home">
+          Inicio
+        </button>
       </div>
     );
   }
