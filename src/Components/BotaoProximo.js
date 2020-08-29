f0d8433c21089625a28179a19be0e4fd0098f4a0
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-08-29 20:37:16 -0300

    Finalmente indo para o Feedback.

--- src/Components/BotaoProximo.js
+++ src/Components/BotaoProximo.js
@@ -1,27 +1,63 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { changeIndex, resetAnswer } from '../Actions';
 
+function ButtonNext(props) {
+  const { disabled, handleClick, indexChange, resetingAnswer, respondido } = props;
+  return (
+    <div>
+      <button
+        className={respondido ? 'button' : 'buttonNull'}
+        data-testid="btn-next"
+        onClick={() => {
+          resetingAnswer();
+          indexChange();
+          handleClick();
+        }}
+      >
+        Próxima
+      </button>
+    </div>
+  );
+}
+
+function ButtonFeedBack({ resetingAnswer, respondido }) {
+  return respondido ? (
+    <Link to="/feedback" className={respondido ? 'button' : 'buttonNull'}>
+      <button
+        className={respondido ? 'button' : 'buttonNull'}
+        onClick={() => {
+          resetingAnswer();
+        }}
+      >
+        Resultado
+      </button>
+    </Link>
+  ) : null;
+}
+
 class BotaoProximo extends Component {
   render() {
-    const { disabled, handleClick, indexChange, resetingAnswer, respondido } = this.props;
-
-    return (
-      <div>
-        <button
-          className={respondido ? 'button' : 'buttonNull'}
-          data-testid="btn-next"
-          disabled={disabled}
-          onClick={() => {
-            resetingAnswer();
-            indexChange();
-            handleClick();
-          }}
-        >
-          Próxima
-        </button>
-      </div>
+    const {
+      disabled,
+      handleClick,
+      indexChange,
+      resetingAnswer,
+      respondido,
+      indexJogo,
+    } = this.props;
+    return indexJogo === 4 ? (
+      <ButtonFeedBack respondido={respondido} disabled={disabled} resetingAnswer={resetingAnswer} />
+    ) : (
+      <ButtonNext
+        disabled={disabled}
+        handleClick={handleClick}
+        indexChange={indexChange}
+        resetingAnswer={resetingAnswer}
+        respondido={respondido}
+      />
     );
   }
 }
@@ -33,6 +69,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 const mapStateToProps = (state) => ({
   respondido: state.answerReducer.respondido,
+  indexJogo: state.indexJogoReducer.indexJogo,
 });
 
 BotaoProximo.propTypes = {
@@ -41,6 +78,7 @@ BotaoProximo.propTypes = {
   indexChange: PropTypes.func.isRequired,
   resetingAnswer: PropTypes.func.isRequired,
   respondido: PropTypes.bool.isRequired,
+  indexJogo: PropTypes.number.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(BotaoProximo);
