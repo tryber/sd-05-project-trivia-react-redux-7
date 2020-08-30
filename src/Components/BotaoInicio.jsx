b70e59ca46e23ba37589e3bfbb80eeacf44ceaa1
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-08-30 19:20:10 -0300

    BABALITY

--- src/Components/BotaoInicio.jsx
+++ src/Components/BotaoInicio.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types;';
+import PropTypes from 'prop-types';
 import { resetPlacar, resetIndex, unblockAnswer, resetAnswer, resetState } from '../Actions';
 
 const buttonHome = ({
