8c7367ce1b087c5b2ae61f09a25349bb2ce51119
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-08-30 00:53:10 -0300

    criando função de salvar no local storage

--- src/Services/saveToLocalStorage.js
+++ src/Services/saveToLocalStorage.js
@@ -1,6 +1,14 @@
-function saveToLocalStorage(name, assertions, score, gravatarEmail) {
+export function saveToLocalStorage(name, assertions, score, gravatarEmail) {
   const toStorage = { player: { name, assertions, score, gravatarEmail } };
   localStorage.setItem('state', JSON.stringify(toStorage));
 }
+
+export function saveStateUserToLocalStorage(player) {
+  const RANKING = 'ranking';
+  const actualStorage = JSON.parse(localStorage.getItem(RANKING))||[];
+  actualStorage.push(player);
+  localStorage.setItem(RANKING);
+}
+
 
 export default saveToLocalStorage;
